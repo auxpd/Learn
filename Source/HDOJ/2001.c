c9ef301ac656ef4ffa80de4058baebe210e451cb
LeanDeR <auxpd96@163.com>
2018-02-04 18:40:36 +0800

    revise 2001.c

--- Source/HDOJ/2001.c
+++ Source/HDOJ/2001.c
@@ -10,7 +10,7 @@ int main(){
     node a;
     node b;
     while(scanf("%f %f %f %f",&a.x ,&a.y ,&b.x ,&b.y) >= 0){
-        printf("%.2f\n",sqrt(((a.x + b.x)*(a.x + b.x)) + ((a.y + b.y)*(a.y + b.y))));
+        printf("%.2f\n",sqrt(((a.x - b.x)*(a.x - b.x)) + ((a.y - b.y)*(a.y - b.y))));
     }
     return 0;
 }
