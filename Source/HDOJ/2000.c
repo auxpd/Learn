e41e6ef2d7a4e3e4cd52fee6972b91b4aa8aa762
LeanDeR <auxpd96@163.com>
2018-02-21 18:04:05 +0800

    add note

--- Source/HDOJ/2000.c
+++ Source/HDOJ/2000.c
@@ -3,18 +3,18 @@
 int main(){
 	char a, b, c, t;
 	while(scanf("%c%c%c",&a,&b,&c) > 0){
-		if(a > b){
+		if(a > b){	//对比a和b，并交换
 			t = a;
 			a = b;
 			b = t;
 		}
-		if(c < a){
+		if(c < a){	//对比c和a，并交换
 			t = a;
 			a = c;
 			c = b;
 			b = t;
 		}
-		if(c > a && c < b){
+		if(c > a && c < b){	//确定c位置
 			t = b;
 			b = c;
 			c = t;
