7245585d7d04426b119c1f25312a2e85018f4503
LeanDeR <auxpd96@163.com>
2018-03-03 21:51:40 +0800

    wait

---
+++ Source/HDOJ-C/2044.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int a, m, n, i;
+	while(scanf("%d", &a) != EOF){
+		scanf("%d %d", &n, &m);
+		for(i = 0; i < (m - n); i++){
+			//回学校在写
+		}
+	}
+	return 0;
+}
