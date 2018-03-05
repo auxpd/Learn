2b8763488c8e8040484a2da6c1ad5f20a502a0e1
LeanDeR <auxpd96@163.com>
2018-03-05 14:46:06 +0800

    add 2044

--- Source/HDOJ-C/2044.c
+++ Source/HDOJ-C/2044.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-	int a, m, n, i;
+	int a, m, n;
+	long long num[50], i;
+	num[0] = 1, num[1] = 2;
 	while(scanf("%d", &a) != EOF){
-		scanf("%d %d", &n, &m);
-		for(i = 0; i < (m - n); i++){
-			//回学校在写
+		while(a--){
+			scanf("%d %d", &n, &m);
+			for(i = 2; i < m - n; i++)
+				num[i] = num[i - 1] + num[i - 2];
+			printf("%lld\n", num[m - n - 1]);
 		}
 	}
 	return 0;
