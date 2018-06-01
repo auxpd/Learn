3ec08b2787a8715f5d22bd30a99ee0f1f585f6dd
LeanDeR <auxpd96@163.com>
2018-06-01 15:28:29 +0800

    2046.c

--- Source/HDOJ-C/2046.c
+++ Source/HDOJ-C/2046.c
@@ -2,8 +2,12 @@
 
 int main(void){
 	int a;
+	long long temp[55] = {0, 1, 2};
+	for(int i = 3; i < 55; i++){
+		temp[i] = temp[i - 1] + temp[i - 2];
+	}
 	while(scanf("%d", &a) != EOF){
-		
+		printf("%lld\n", temp[a]);	
 	}
 	return 0;
 }
